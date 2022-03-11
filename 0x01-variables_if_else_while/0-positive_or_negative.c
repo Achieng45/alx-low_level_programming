ca91fde856b61b984a3dbeca000ba64ae36da3f2
Achieng45 <stephanieachieng45@gmail.com>
2022-03-11 02:08:45 -0800

    random

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,13 +6,14 @@ int main(void)
 	int n;
 	srand(time(0));
 	n=rand()-RAND_MAX/2;
-	if(n>0){
+	if(n>0)
 		printf("is positive.\n");
-	        }
-	else if(n<0){
+	        
+	else if(n<0)
 		printf("is negative.\n");
-	}
-else {
+	
+else 
 printf("is negative.\n");
 return(0);
-}}
+}
+
