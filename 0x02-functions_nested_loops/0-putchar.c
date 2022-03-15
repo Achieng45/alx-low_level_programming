4bcbec5130dc78370629ef2e9468adc11d0879e7
Stephanie Achieng <stephanieachieng45@gmail.com>
2022-03-15 22:23:27 +0300

    N

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,21 @@
-#include<iostream>
-int main()
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
 {
-	char put="_putchar";
-	putchar(put);
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
 }
