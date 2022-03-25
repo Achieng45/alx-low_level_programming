9152982c6032501b48ab2ee33e16a5e5acdcd452
Achieng45 <stephanieachieng45@gmail.com>
2022-03-25 23:10:17 +0300

    2-strn

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,31 +1,34 @@
 #include "main.h"
 
 /**
- *print_number - print a number using _putchar.
- *@n: the number to be printed.
+ *_strncpy - copy src into dest.
+ *@dest: first string.
+ *@src: second string.
+ *@n: the number of bytes to use from src.
+ *Return: string.
  */
 
-void print_number(int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	unsigned int i = 1;
+	int i = 0, k = 0;
 
-	if (n < 0)
+	while (n > k)
 	{
-		_putchar('-');
-		n *= -1;
-	}
-	if (n == 0)
-		_putchar('0');
-	else
-	{
-		while ((n / i) >= 10)
-			i *= 10;
-
-		while (i > 0)
+		if (src[k] == '\0')
 		{
-			_putchar((n / i) + '0');
-			n %= i;
-			i /= 10;
+			for (; k < n; k++)
+			{
+				dest[i] = '\0';
+				i++;
+			}
+		}
+		else
+		{
+			dest[i] = src[k];
+			k++;
+			i++;
 		}
 	}
+
+	return (dest);
 }
