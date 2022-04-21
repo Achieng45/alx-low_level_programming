79c819de20ea10ba9069e04da5cceb740e5202ca
Stephanie Achieng <stephanieachieng45@gmail.com>
2022-04-22 01:44:34 +0300

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,16 @@
-.MODEL SMALL 
-.STACK 100H 
-.DATA 
-  
-;The string to be printed 
-STRING DB 'Hello, Holberton', '$'
-  
-.CODE 
-MAIN PROC FAR 
- MOV AX,@DATA 
- MOV DS,AX 
-  
- ; load address of the string 
- LEA DX,STRING 
-  
- ;output the string
- ;loaded in dx 
- MOV AH,09H
- INT 21H 
-  
- ;interrupt to exit
- MOV AH,4CH
- INT 21H 
-  
-MAIN ENDP 
-END MAIN 
+section .data
+	text db "Hello, Holberton",10
+
+	section .text
+	global main
+
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text
+	mov rdx, 17
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
