e6adea5c698ea4f7759e28d4aa68063929bc2aa1
Stephanie Achieng <stephanieachieng45@gmail.com>
2022-04-25 20:04:36 +0300

    betty

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,17 +3,18 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
     listint_t *add;
-    size_t nchar;
+    
     
     add=malloc(sizeof(listint_t))
     if(add==NULL)
-        return(NULL);
+          return(NULL);
      else
         return **add;
      
-     for(nchar=0;str[nchar];nchar++);
-     add->n=nchar;
+     
+     add->n=n;
      add->next=*head;
+     *head=add;
      return(*head);
     
 }
