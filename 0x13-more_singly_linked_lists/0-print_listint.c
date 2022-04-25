3e107eea65dac6ae5e6df617a9b908097bc4a6a2
Stephanie Achieng <stephanieachieng45@gmail.com>
2022-04-25 19:18:53 +0300

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@ size_t print_listint(const listint_t *h)
     nodel=0;
     while(h!=NULL)
     {
-        printf("[%d]%s\n",h->n);
+        printf("%d\n",h->n);
         h=h->next;
         nodel++;
 
