c10a618f431346fdd97d42921c349f50d5254f56
Stephanie Achieng <stephanieachieng45@gmail.com>
2022-04-26 18:24:38 +0300

    add

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,9 +3,9 @@ void free_listint(listint_t *head)
 {
     listint_t *current;
     while((current=head)!=NULL)
-    {
+    {   
+	current=*head;
         head=head->next;
-        
         free(current);
     }
 }
