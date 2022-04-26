942a803fe04da04b4195a1847737bad2db0ecd44
Stephanie Achieng <stephanieachieng45@gmail.com>
2022-04-26 07:50:43 +0300

    add

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,32 +8,33 @@
  *
  * Return: address of the new element. NUll if it failed.
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new,*temp;
-    new=malloc(sizeof(listint_t));
-    if(new==NULL)
-    {
-        return(NULL);
-    new->n=n;
-    new->next=NULL;
-    temp=*head;
-        
-    }
-    if(temp==NULL)
-    {
-        *head=new;
-    
-        
-    }
-    else
-    {
-        while(temp->next!=NULL)
-        {
-            temp=temp->next;
-        }
-        temp->next=new;
-    }
-    return(*head);
+	listint_t *new;
+	listint_t *temp;
+
+	(void)temp;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	temp = *head;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
+
+	return (*head);
 }
