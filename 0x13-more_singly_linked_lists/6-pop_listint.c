b214e93eb79197d2929b1cf9aeff24271596c32d
Stephanie Achieng <stephanieachieng45@gmail.com>
2022-04-26 20:10:32 +0300

    add

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,17 +10,21 @@
 int pop_listint(listint_t **head)
 {
 	int hnode;
-	listint_t *curr,*h;
-	if(*head==NULL)
-		return(NULL);
-	cur=*head;
-	hnode=cur->n;
-	h=curr->next;
-	
+	listint_t *h;
+	listint_t *curr;
+
+	if (*head == NULL)
+		return (0);
+
+	curr = *head;
+
+	hnode = curr->n;
+
+	h = curr->next;
+
 	free(curr);
-	h=*head;
-	return(hnode);
 
+	*head = h;
+
+	return (hnode);
 }
-
-
