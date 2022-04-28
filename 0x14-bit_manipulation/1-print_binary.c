d8fedbbb03e6560e72bab571cd7d0c55cc0966a0
Stephanie <stephanie@stephanie-VirtualBox>
2022-04-28 21:06:30 +0300

    ed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 void print_binary(unsigned long int n)
 {
+    int func;
     if(n>1)
+    {
         print_binary(n/2);
-    putchar(n%2);
+        func=n&2;
+     putchar(func);
+    }
 }
